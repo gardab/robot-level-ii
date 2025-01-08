8e62a04218ce5a19d5918e594a8cdd9193c36114
Felipe Brubeck <felipe.brubeck@gmail.com>
2025-01-08 16:56:43 -0600

    Added functionality to fill and submit order form

--- tasks.py
+++ tasks.py
@@ -16,9 +16,12 @@ def order_robots_from_RobotSpareBin():
     browser.configure(
         slowmo=100,
     )
-    open_robot_order_website()
     robot_orders = get_orders()
-    print(robot_orders)
+    open_robot_order_website()
+    close_annoying_modal()
+    for order in robot_orders:
+        fill_the_form(order)
+    
     
 def open_robot_order_website():
     """Navigates to the given URL"""
@@ -30,7 +33,23 @@ def get_orders():
     http.download(url="https://robotsparebinindustries.com/orders.csv", overwrite=True)
     library = Tables()
     robot_orders = library.read_table_from_csv(
-        "orders.csv", header=True, columns=["Order Number", "Head", "Body", "Legs", "Address"]
+        "orders.csv", header=True, columns=["Order number", "Head", "Body", "Legs", "Address"]
     )
     return robot_orders
 
+def close_annoying_modal():
+    """Clicks on the OK button on the annoying modal that pops up
+    """
+    page = browser.page()
+    page.click("button:text('OK')")
+    
+def fill_the_form(order):
+    """Fill the form to order a robot with the details from the csv and clicks on the preview button
+    """
+    page = browser.page()
+    page.select_option("select#head", order['Head'])
+    page.check(f"#id-body-{order['Body']}")
+    page.fill("input[placeholder='Enter the part number for the legs']", order['Legs'])
+    page.fill("#address", order['Address'])
+    page.click("text=Preview")
+    page.click("text=ORDER")
